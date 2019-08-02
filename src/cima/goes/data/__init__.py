ff90bbab55cc38b85b7db7d1853684a2f9646843
Fido García <garciafido@gmail.com>
2019-08-02 18:04:16 -0300

    Initial

--- src/cima/goes/data/__init__.py
+++ src/cima/goes/data/__init__.py
@@ -1,2 +1,2 @@
 from cima.goes.data._data import get_data, gamma_correction, compose_RGB
-from cima.goes.data.palettes import CLOUD_TOPS_PALETTE
+from cima.goes.data._palettes import CLOUD_TOPS_PALETTE
