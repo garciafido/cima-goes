057c58ca68aa008dad833794d1135e1d18114b5a
Fido García <garciafido@gmail.com>
2019-08-02 15:33:07 -0300

    Initial

--- src/cima/goes/tiles/__init__.py
+++ src/cima/goes/tiles/__init__.py
@@ -1,1 +1,1 @@
-from cima.goes.tiles._tiles import Tile, BandTiles, load_tiles, save_tiles, generate_tiles
+from cima.goes.tiles._tiles import Tile, BandTiles, load_tiles, save_tiles, generate_tiles, get_tile_extent
