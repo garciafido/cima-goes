c4071a42339136da5934838ac010b210049cf313
Fido García <garciafido@gmail.com>
2019-08-05 09:19:36 -0300

    Initial

--- src/cima/goes/storage/__init__.py
+++ src/cima/goes/storage/__init__.py
@@ -2,3 +2,4 @@ from cima.goes.storage._ftp import FTP
 from cima.goes.storage._async_ftp import AFTP
 from cima.goes.storage._nfs import NFS
 from cima.goes.storage._gcs import GCS
+from cima.goes.storage._factories import mount_goes_storage, mount_storage
