615e05abfdd4d8cce0e709a7c804d877503916a2
Fido García <garciafido@gmail.com>
2019-08-06 14:12:36 -0300

    Initial

--- src/cima/goes/__init__.py
+++ src/cima/goes/__init__.py
@@ -1,3 +1,1 @@
 from cima.goes.utils._file_names import Band, Product, ProductBand, ANY_MODE
-from cima.goes.storage._blobs import GroupedBandBlobs
-from cima.goes.storage._goes_data import GoesStorage
--- src/cima/goes/storage/__init__.py
+++ src/cima/goes/storage/__init__.py
@@ -3,3 +3,5 @@ from cima.goes.storage._async_ftp import AFTP
 from cima.goes.storage._nfs import NFS
 from cima.goes.storage._gcs import GCS
 from cima.goes.storage._factories import mount_goes_storage, mount_storage
+from cima.goes.storage._blobs import GroupedBandBlobs
+from cima.goes.storage._goes_data import GoesStorage
