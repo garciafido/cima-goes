212a8ef0a020e7cec6567ced59556037d054b636
Fido García <garciafido@gmail.com>
2019-08-06 11:55:27 -0300

    Initial

--- src/cima/goes/img/_images.py
+++ src/cima/goes/img/_images.py
@@ -111,8 +111,7 @@ def save_image(image,
             format = file_extension[1:]
         plt.savefig(buffer, format=format, dpi=image_inches.dpi, bbox_inches='tight', pad_inches=0)
         buffer.seek(0)
-        plt.show()
-        storage.upload_stream(buffer.read(), filepath)
+        storage.upload_stream(plt.imread(buffer), filepath)
     finally:
         fig.clear()
         plt.close()
