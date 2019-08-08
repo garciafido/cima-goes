1812abfc8e867593976ed36d375901620019cc8e
Fido García <garciafido@gmail.com>
2019-08-08 19:57:36 -0300

    Initial

--- src/cima/goes/projects/_project.py
+++ src/cima/goes/projects/_project.py
@@ -28,8 +28,9 @@ class DatesRange:
 ProcessCall = Callable[[GoesStorage, int, int, int, int, int, List[BandBlobs], List[Any], Dict[str, Any]], Any]
 
 
-def _call(storage_info: StorageInfo, process: ProcessCall, year, month, day, hour, minute, blobs, *args, **kwargs):
+def _call(storage_info: StorageInfo, process: ProcessCall, year, month, day, hour, minute, blob_names, *args, **kwargs):
     goes_storage = mount_goes_storage(storage_info)
+    blobs = [goes_storage.bucket.blob(name) for name in blob_names]
     process(goes_storage,
             year, month, day, hour, minute, blobs,
             *args, **kwargs)
