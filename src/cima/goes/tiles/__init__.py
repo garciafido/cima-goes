4bda7114c91729422a4c190152d928cc41058442
Fido García <garciafido@gmail.com>
2019-08-08 13:39:44 -0300

    Initial

--- src/cima/goes/tiles/__init__.py
+++ src/cima/goes/tiles/__init__.py
@@ -3,4 +3,4 @@ from cima.goes.tiles._dataset_region import find_dataset_region, dataset_region_
 from cima.goes.tiles._dataset_region import get_tiles, tiles_to_dict, TilesDict, dataset_key_as_string
 from cima.goes.tiles._dataset_region import region_data_from_dict, get_dataset_key, dict_to_tiles, get_tile_extent
 from cima.goes.tiles._dataset_region import save_tiles, save_region_data, load_tiles, load_region_data, RegionData
-from cima.goes.tiles._dataset_region import get_data, generate_region_data, get_dataset_region
+from cima.goes.tiles._dataset_region import get_data, generate_region_data, get_dataset_region, get_lats_lons
