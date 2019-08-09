ea7eeba946e8ebe3c85419963a599322e2083ddc
Fido García <garciafido@gmail.com>
2019-08-09 12:49:11 -0300

    Initial

--- src/cima/goes/projects/_project.py
+++ src/cima/goes/projects/_project.py
@@ -74,7 +74,7 @@ class BatchProcess(object):
         self.date_ranges = date_ranges
         self.goes_storage = goes_storage
 
-    def run(self, process: ProcessCall, *args, workers=2, storage: Storage=None, **kwargs):
+    def run(self, process: ProcessCall, *args, workers=1, storage: Storage=None, **kwargs):
         def dates_range(date_range: DatesRange):
             current_date = date_range.from_date
             last_date = date_range.to_date
