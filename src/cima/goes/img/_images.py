e8ebf0520d603745da2e51f24a5533c769304e24
Fido García <garciafido@gmail.com>
2019-08-09 18:10:42 -0300

    Initial

--- src/cima/goes/img/_images.py
+++ src/cima/goes/img/_images.py
@@ -10,64 +10,64 @@ from cima.goes.storage._file_systems import Storage
 from cima.goes.tiles import DatasetRegion, LatLonRegion, get_tile_extent
 from cima.goes.utils.load_cpt import load_cpt
 from matplotlib.axes import Axes
-from PIL import Image
+# from PIL import Image
 
 
 LOCAL_BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
-def fig2data(fig):
-    """
-    @brief Convert a Matplotlib figure to a 4D numpy array with RGBA channels and return it
-    @param fig a matplotlib figure
-    @return a numpy 3D array of RGBA values
-    """
-    # draw the renderer
-    fig.canvas.draw()
-
-    # Get the RGBA buffer from the figure
-    w, h = fig.canvas.get_width_height()
-    buf = np.frombuffer(fig.canvas.tostring_argb(), dtype=np.uint8)
-    buf.shape = (w, h, 4)
-
-    # canvas.tostring_argb give pixmap in ARGB mode. Roll the ALPHA channel to have it in RGBA mode
-    buf = np.roll(buf, 3, axis=2)
-    return buf
-
-
-def fig2data(fig):
-    """
-    @brief Convert a Matplotlib figure to a 4D numpy array with RGBA channels and return it
-    @param fig a matplotlib figure
-    @return a numpy 3D array of RGBA values
-    """
-    # draw the renderer
-    fig.canvas.draw()
-
-    # Get the RGBA buffer from the figure
-    w, h = fig.canvas.get_width_height()
-    buf = np.frombuffer(fig.canvas.tostring_argb(), dtype=np.uint8)
-    buf.shape = (w, h, 4)
-
-    # canvas.tostring_argb give pixmap in ARGB mode. Roll the ALPHA channel to have it in RGBA mode
-    buf = np.roll(buf, 3, axis=2)
-    return buf
-
-
-def fig2img(fig):
-    """
-    @brief Convert a Matplotlib figure to a PIL Image in RGBA format and return it
-    @param fig a matplotlib figure
-    @return a Python Imaging Library ( PIL ) image
-    """
-    # put the figure pixmap into a numpy array
-    buf = fig2data(fig)
-    w, h, d = buf.shape
-    # im = Image.open(cStringIO.StringIO(buf))
-    # return Image.frombuffer("RGBA", (w ,h), buf.tostring())
-    return Image.open("RGBA", buf.tostring())
-
-
+# def fig2data(fig):
+#     """
+#     @brief Convert a Matplotlib figure to a 4D numpy array with RGBA channels and return it
+#     @param fig a matplotlib figure
+#     @return a numpy 3D array of RGBA values
+#     """
+#     # draw the renderer
+#     fig.canvas.draw()
+#
+#     # Get the RGBA buffer from the figure
+#     w, h = fig.canvas.get_width_height()
+#     buf = np.frombuffer(fig.canvas.tostring_argb(), dtype=np.uint8)
+#     buf.shape = (w, h, 4)
+#
+#     # canvas.tostring_argb give pixmap in ARGB mode. Roll the ALPHA channel to have it in RGBA mode
+#     buf = np.roll(buf, 3, axis=2)
+#     return buf
+#
+#
+# def fig2data(fig):
+#     """
+#     @brief Convert a Matplotlib figure to a 4D numpy array with RGBA channels and return it
+#     @param fig a matplotlib figure
+#     @return a numpy 3D array of RGBA values
+#     """
+#     # draw the renderer
+#     fig.canvas.draw()
+#
+#     # Get the RGBA buffer from the figure
+#     w, h = fig.canvas.get_width_height()
+#     buf = np.frombuffer(fig.canvas.tostring_argb(), dtype=np.uint8)
+#     buf.shape = (w, h, 4)
+#
+#     # canvas.tostring_argb give pixmap in ARGB mode. Roll the ALPHA channel to have it in RGBA mode
+#     buf = np.roll(buf, 3, axis=2)
+#     return buf
+#
+#
+# def fig2img(fig):
+#     """
+#     @brief Convert a Matplotlib figure to a PIL Image in RGBA format and return it
+#     @param fig a matplotlib figure
+#     @return a Python Imaging Library ( PIL ) image
+#     """
+#     # put the figure pixmap into a numpy array
+#     buf = fig2data(fig)
+#     w, h, d = buf.shape
+#     # im = Image.open(cStringIO.StringIO(buf))
+#     # return Image.frombuffer("RGBA", (w ,h), buf.tostring())
+#     return Image.open("RGBA", buf.tostring())
+#
+#
 def _resize(image, new_size):
   return cv2.resize(image, dsize=new_size, interpolation=cv2.INTER_CUBIC)
 
