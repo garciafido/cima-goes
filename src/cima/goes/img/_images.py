614850667191d6cb9898be785e02d0040e3ebabd
Fido García <garciafido@gmail.com>
2019-12-26 13:56:25 -0300

    testing pcolormesh error

--- src/cima/goes/img/_images.py
+++ src/cima/goes/img/_images.py
@@ -125,15 +125,13 @@ def make_color_tuple(rgb):
 
 
 def pcolormesh(ax: Axes, image, lons, lats, cmap=None, vmin=None, vmax=None):
-    image_m = np.ma.masked_where(np.isnan(image), image)
-    image_m = np.ma.masked_invalid(image_m)
     if len(image.shape) == 3:
-        color_tuple = make_color_tuple(image_m)
+        color_tuple = make_color_tuple(image)
         # ax.pcolormesh(lons, lats, np.zeros_like(lons),
         #               color=color_tuple, linewidth=0)
-        ax.pcolormesh(lons, lats, image_m[:, :, 0], color=color_tuple)
+        ax.pcolormesh(lons, lats, image[:, :, 0], color=color_tuple)
     else:
-        ax.pcolormesh(lons, lats, image_m, cmap=cmap, vmin=vmin, vmax=vmax)
+        ax.pcolormesh(lons, lats, image, cmap=cmap, vmin=vmin, vmax=vmax)
 
 
 def set_extent(ax: Axes, lonlat_region: LatLonRegion, trim_excess=0):
