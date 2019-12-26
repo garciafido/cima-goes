3f56aa9696b8e48d079147d9d0a7735a296710a4
Fido García <garciafido@gmail.com>
2019-12-26 13:24:45 -0300

    testing pcolormesh error

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup
 
 setup(
     name='cima.goes',
-    version='1.1.b66',
+    version='1.1.b67',
     description='GOES-16 File Processing',
     author='Fido Garcia',
     author_email='garciafido@gmail.com',
--- src/cima/goes/img/_images.py
+++ src/cima/goes/img/_images.py
@@ -125,7 +125,7 @@ def make_color_tuple(rgb):
 
 
 def pcolormesh(ax: Axes, image, lons, lats, cmap=None, vmin=None, vmax=None):
-    if len(image.shape) == 3:
+    if len(image.shape) >= 3:
         color_tuple = make_color_tuple(image)
         # ax.pcolormesh(lons, lats, np.zeros_like(lons),
         #               color=color_tuple, linewidth=0)
